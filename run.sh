67b9ab16a427fc7052cee36a85b59f942df8f038
User <17693119+vindard@users.noreply.github.com>
2019-12-20 17:33:50 -0400

    take an ssh config file and extract servers for update

--- run.sh
+++ run.sh
@@ -3,15 +3,25 @@
 # DIR=
 SCRIPT=addusers.sh
 USERS_FILE="$1"
-SERVER="$2"
+SSH_CONFIG_FILE="$2"
 
-scp "$SCRIPT" "$SERVER":"$DIR"
-scp "$USERS_FILE" "$SERVER":"$DIR"
+adduser () {
+	SSH_PATH=$1
 
-ssh "$SERVER" bash <<-EOF
-	cd ~/"$DIR"
-	sudo chmod u+x "$SCRIPT"
-	./"$SCRIPT" "$USERS_FILE"
-	rm "$SCRIPT" "$USERS_FILE"
-EOF
+	scp "$SCRIPT" "$SSH_PATH":"$DIR"
+	scp "$USERS_FILE" "$SSH_PATH":"$DIR"
 
+	ssh "$SSH_PATH" bash <<-EOF
+		cd ~/"$DIR"
+		sudo chmod u+x "$SCRIPT"
+		./"$SCRIPT" "$USERS_FILE"
+		rm "$SCRIPT" "$USERS_FILE"
+	EOF
+}
+
+for ssh_path in $(cat $SSH_CONFIG_FILE | grep '^Host \*\w' | awk '{print $2;}' | cut -c 2-); do
+	ssh_user_and_path='admin.'$ssh_path
+    echo "Accessing $ssh_user_and_path" && echo "---"
+	adduser $ssh_user_and_path
+	echo && echo "===================================" && echo
+done
