4befa19ac87ba782da94a50291cc414621d9447a
User <17693119+vindard@users.noreply.github.com>
2020-01-06 14:37:16 -0400

    set config file as script default instead of arg passed

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 
 # DIR=
 SCRIPT=addusers.sh
+SSH_DIR=$HOME/.ssh
+SSH_CONFIG_FILE=$SSH_DIR/config
 USERS_FILE="$1"
-SSH_CONFIG_FILE="$2"
 
 adduser () {
 	SSH_PATH=$1
@@ -19,9 +20,17 @@ adduser () {
 	EOF
 }
 
-for ssh_path in $(cat $SSH_CONFIG_FILE | grep '^Host \*\w' | awk '{print $2;}' | cut -c 2-); do
-	ssh_user_and_path='admin.'$ssh_path
-    echo "Accessing $ssh_user_and_path" && echo "---"
-	adduser $ssh_user_and_path
-	echo && echo "===================================" && echo
-done
+run_add () {
+	for ssh_path in $(cat $SSH_CONFIG_FILE | grep '^Host \*\w' | awk '{print $2;}' | cut -c 2-); do
+		ssh_user_and_path='admin.'$ssh_path
+	    echo "Accessing $ssh_user_and_path" && echo "---"
+		adduser $ssh_user_and_path
+		echo && echo "===================================" && echo
+	done
+}
+
+if [ -f $SSH_CONFIG_FILE ]; then
+	run_add
+else
+	echo "Please generate an ssh config file in the expected format at '~/.ssh/config' and then rerun script"
+fi
